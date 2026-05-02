1e9f61ebf2dc066a75c498601c87a5668212917c
Knight <naitokato@gmail.com>
2026-05-02 21:24:46 +0900

    26/5/2　GetEnemyNumの追加
    なんか不具合があれば、引数にtrueを入れてください

--- Assets/2_KNFolder/Script/EnemyManager.cs
+++ Assets/2_KNFolder/Script/EnemyManager.cs
@@ -19,6 +19,21 @@ public class EnemyManager : MonoBehaviour
             Destroy(gameObject);
     }
 
+    public int GetEnemyNum(bool refresh = false)
+    {
+        if (refresh)
+        {
+            List<EnemyBase> list = new();
+            foreach(EnemyBase enemy in _enemies)
+            {
+                if (enemy)
+                    list.Add(enemy);
+            }
+            return list.Count;
+        }
+
+        return _enemies.Count;
+    }
     public void Register(EnemyBase enemy)
     {
         if (enemy && !_enemies.Contains(enemy))
