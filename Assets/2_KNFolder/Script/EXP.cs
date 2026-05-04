36d138a1c435d47b3f3e874406c32599c4f81b59
Knight <naitokato@gmail.com>
2026-05-04 09:13:52 +0900

    26/5/4　経験値の吸い寄せ

--- Assets/2_KNFolder/Script/EXP.cs
+++ Assets/2_KNFolder/Script/EXP.cs
@@ -1,6 +1,32 @@
+using Player;
 using UnityEngine;
 
 public class EXP : MonoBehaviour
 {
     public int Point;
+
+    private Rigidbody rb;
+
+    private float time;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        Vector3 vec = PlayerCore.Instance.gameObject.transform.position - transform.position;
+        if (time >= 1)
+        {
+            if (vec.magnitude <= 2)
+            {
+                rb.linearVelocity = vec.normalized * time;
+                time += Time.fixedDeltaTime;
+            }
+        }
+        else
+        {
+            time += Time.fixedDeltaTime;
+        }
+    }
 }
